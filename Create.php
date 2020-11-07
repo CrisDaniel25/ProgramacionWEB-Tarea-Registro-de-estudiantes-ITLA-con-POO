b2593f44748442dd2eaff6612752c6f8c54faea3
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-11-07 08:46:00 -0400

    commit

---
+++ Create.php
@@ -0,0 +1,5 @@
+<?php 
+$cookie_name = 'pontikis_net_php_cookie';
+$cookie_value = 'test_cookie_set_with_php';
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), '/');
+?>
--- Index.php
+++ Index.php
@@ -5,17 +5,123 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Programacion WEB Tarea Registro de estudiantes ITLA con POO</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="Style.css">
+     <link rel="stylesheet" href="Style.css">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
 </head>
 <body>
 <header>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" id="tittle">
-  Programacion WEB Tarea Registro de estudiantes ITLA
+  Programacion WEB Tarea Registro de estudiantes ITLA con POO
   <span>  By Cristopher Figueroa | Matricula: 2018-6186</span>
   </a>
 </nav>
 </header>
+
+<div class="container">
+<div class="row">
+
+<form action="Create.php" method="post">
+<div class="form-group">
+  <div class="input-group">
+    <div class="input-group-prepend">
+      <div class="input-group-text bg-white">
+        <i class="fas fa-user-circle"></i>
+      </div>
+    </div>
+    <input class="form-control" placeholder="Nombre" name="nombre" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <div class="input-group">
+    <div class="input-group-prepend">
+      <div class="input-group-text bg-white">
+        <i class="fas fa-user-circle"></i>
+      </div>
+    </div>
+    <input class="form-control" placeholder="Apellido" name="apellido" type="text">
+  </div>
+</div>
+
+<div class="form-group">  
+  <input type="radio" name="status" value="Activo"> Activo 
+</div> 
+
+<div class="form-group">
+  <div class="input-group">
+    <div class="input-group-prepend">
+      <div class="input-group-text bg-white">
+        <i class="fas fa-university"></i>
+      </div>
+    </div>
+    <input class="form-control" name="carrera" placeholder="Carrera" type="text">
+  </div>
+</div>
+<button type="submit" name="create" value="Create" class="btn btn-success">
+  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+  </svg>
+</button>
+<button type="reset" value="Clear" class="btn btn-danger"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+</svg>
+</button>
+</form>
+
+<!-- <?php
+$cookie_name = 'pontikis_net_php_cookie';
+if(!isset($_COOKIE[$cookie_name])) {
+        print 'Cookie with name "' . $cookie_name . '" does not exist...';
+} else {
+        print 'Cookie with name "' . $cookie_name . '" value is: ' . $_COOKIE[$cookie_name];
+}
+?> -->
+
+<table class="table table-hover">
+	<thead class="thead-dark">
+    <th>Id</th>
+    <th>Nombre</th>
+    <th>Status</th>
+    <th>Carrera</th>
+    <th>Materias</th>
+    <th>Foto</th>
+    <th></th>
+    <th></th>
+  </thead>
+  <tbody>
+  <tbody>
+  <?php 
+			if(!empty($allData)){
+				foreach ($allData as $key => $value) {
+		 ?>
+    <tr>
+      <td scope><?php echo $key+1;?></td>
+			<td><?php echo $value['nombre'];?></td>
+			<td><?php echo $value['apellido'];?></td>
+			<td><?php echo $value['status'];?></td>
+			<td><?php echo $value['carrera']; ?></td>
+      <td>
+<a href="Delete.php?id=<?php echo $key;?>" class="btn btn-danger"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+</svg>
+</a>
+<a href="Edit.php?id=<?php echo $key;?>" class="btn btn-warning"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+  </svg>
+</a>
+      </td>
+    </tr>
+    <?php 
+				}
+			}
+		 ?>
+  </tbody>
+  </tbody>
+</table>
+</div>
+</div>
 
 <footer class="page-footer font-small special-color-dark pt-4"> 
   <div class="container-fluid text-center text-md-left">
